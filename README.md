40c80d544b43cf15d5342d296cd197ef4b294b63
Yohrog <yohrog@gmail.com>
2021-12-29 05:01:39 +0100

    Update readme, rename license file, add preprocessor options and even more operators

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # VecTooDee
 A single header vector math library designed to be used as a bridge between 2D vector implementations
+
+# Why does this library exist?
+When I was trying out different game engines and corresponding physics engines in 
+C++ I noticed that all of them had different implementations for the same thing:
+
+    struct 2DVector
+    {
+        Foo x;
+        Foo y;
+        
+        // and a bunch of member functions for vector math
+    };
+
+I was switching libraries quite a bit I was always spending some time 
+writing some sort of conversion code between the two implementations of the exact same thing.
+Long story short I was getting annoyed because I had to reimplement an adapter class every time. <br>
+Because of that I made this library and implemented it in a single header file, 
+so it is quickly and easily imported into any project. <br>
+Some notable libraries whose coexistence in my code inspired me to create this and that follow the pattern:
+- SFML (sf::Vector2) 
+- Box2D (b2Vec2)
+- Raylib (Vector2)
+- Chipmunk2D (cpVect)
+
+# Basic Usage
+This library can convert any 2D vector implementation that follows the pattern shown above into another one.
+```C++
+    struct Some2DVector {int x, float y};    
+    struct anotherVector {double x, long y};
+
+    Some2DVector someVector;
+    Another2DVector anotherVector;
+
+    Some2DVector convertedVector = vectoo::convert<someVector>(anotherVector);
+    Some2DVector combinedVector = vectoo::add(someVector, anotherVector);
+```
+Additionally, the "vectoo" namespace provides many basic and some more specialized math functions, 
+that might be useful to someone (they were for me, which is why they're here). <br>
+Please note that actually converting vectors of different types into one another does nothing more than cast the values. <br>
+Therefore, the behaviour is the same as when casting the x and y components of one vector into those of the other. 
+
+If for some reason two vector implementations aren't enough for you, I included my own in the library as well:
+```C++
+    // There are shortcuts for common vector types. vec2f -> float, vec2i -> int, vec2u -> unsigned int
+    vec2f anotherFloatVector;
+
+    // The template will accept any arithmetic type and even another vector implementation to match
+    vec2<float> floatVector;
+
+    // This will set x and y to match the ones from the provided vector implementation
+    vec2<Some2DVector> inheritedTypeVector;
+    
+    // And it also has multiple constructors, that will take all expected arguments including a different vector
+    vec2<Some2DVector> convertedVector = vec2(someVector);
+
+    // If not disabled it will also inherit all functions from the vector provided via template or constructor
+    convertedVector.some2DVectorFunction();
+
+    // When the corresponding macro is defined, it will implicitly convert to any other vector and back easily
+    someVector = vec2(anotherVector)
+
+    // And of course it will provide the basic operators that you would expect (+,-,*,/)
+    auto combinedVector = convertedVector + inheritedTypeVector;
+    auto scaledVector = combinedVector * 2;
+```
+# Configuration options
+While it will function in its entirety without configuration, 
+the library does include some preprocessor macros that can be used,
+either for compatibility or for tweaking its functionality:
+
+| **Preprocessor Option**        | **Description**                                                                                                                                                             |
+|--------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| **USE_DOUBLE_PRECISION**       | The floating point return values of functions will be double precision floats                                                                                               |
+| **WITHOUT_VECTOR_INHERITANCE** | The vec2 struct will no longer be a child class of the vector struct it was created from                                                                                    |
+| **ALLOW_IMPLICIT_CONVERSION**  | The vec2 struct will convert to any other vector with implicit conversion.<br> This can be very neat for calling functions with it as parameter, but can be harder to debug |
+| **WITHOUT_OPERATORS**          | The vec2 struct will no longer provide any operators, making those in the provided base struct accessible                                                                   |
+| **WITHOUT_MEMBER_FUNCTIONS**   | Removes all member functions from the vec2 struct, so they don't hide base class functions                                                                                  |
+| **WITHOUT_VECTOR_FUNCTIONS**   | This will remove the vectoo namespace and all functions within, leaving only the vec2 struct                                                                                |
+
+These can be either passed as -D parameter during compilation or be '#define'-d before including the header file.
+
+# TODOs
+- Better support for integer vectors (both signed and unsigned)
+- More options for defining the exact type of floats that are to be used
+- More uniform naming scheme for functions
--- include/vec2d.hpp
+++ include/vec2d.hpp
@@ -1,13 +1,49 @@
+/* Copyright (c) 2021 Yohrog
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #pragma once
 
 #include <cmath>
 #include <ostream>
 #include <type_traits>
 
-
+#ifdef USE_DOUBLE_PRECISION
+using vFloat = double;
+#define vSin sin
+#define vCos cos
+#define vAcos acos
+#define vSqrt sqrt
+#define vAtan2 atan2
+#define vAbs abs
+#else
 using vFloat = float;
+#define vSin sinf
+#define vCos cosf
+#define vAcos acosf
+#define vSqrt sqrtf
+#define vAtan2 atan2f
+#define vAbs fabs
+#endif
 
-
+#ifdef WITHOUT_VECTOR_INHERITANCE
 template <typename ArithmeticType>
 struct IndependentVector2
 {
@@ -15,6 +51,19 @@ struct IndependentVector2
     ArithmeticType y;
 };
 
+template <class Vector>
+struct InheritingVector2
+{
+    using Vector::x;
+    using Vector::y;
+};
+#else
+template <typename ArithmeticType>
+struct IndependentVector2
+{
+    ArithmeticType x;
+    ArithmeticType y;
+};
 
 template <class Vector>
 struct InheritingVector2 : Vector
@@ -22,11 +71,11 @@ struct InheritingVector2 : Vector
     using Vector::x;
     using Vector::y;
 };
+#endif
 
+template <class T=vFloat>
+struct vec2 : public std::conditional<std::is_arithmetic<T>::value, IndependentVector2<T>, InheritingVector2<T>>::type {
 
-template <class T>
-struct vec2 : public std::conditional<std::is_arithmetic<T>::value, IndependentVector2<T>, InheritingVector2<T>>::type
-{
     vec2() {this->x = 0; this->y = 0;};
     vec2(const vec2& vec) {this->x = std::move(vec.x); this->y = std::move(vec.y);};
     template<class Value>
@@ -34,12 +83,20 @@ struct vec2 : public std::conditional<std::is_arithmetic<T>::value, IndependentV
 
     template<class Vector>
     explicit vec2(const Vector& vec) {this->x = std::move(vec.x); this->y = std::move(vec.y);};
-    vec2(const vec2&& vec)  noexcept {this->x = std::move(vec.x); this->y = std::move(vec.y);};
+    vec2(const vec2&& vec) noexcept {this->x = std::move(vec.x); this->y = std::move(vec.y);};
 
+#ifdef ALLOW_IMPLICIT_CONVERSION
+    template<class Vector>
+    operator Vector() const {
+        return Vector {static_cast<typeof(Vector::x)>(this->x), static_cast<typeof(Vector::y)>(this->y)};
+    };
+#else
     template<class Vector>
     explicit operator Vector() const {return Vector {static_cast<typeof(Vector::x)>(this->x),
                                                      static_cast<typeof(Vector::y)>(this->y)};};
+#endif
 
+#ifndef WITHOUT_OPERATORS
     template<class Vector>
     vec2& operator=(const Vector& vec) {this->x = std::move(vec.x); this->y = std::move(vec.y); return *this;};
     vec2& operator=(const vec2& vec) {this->x = std::move(vec.x); this->y = std::move(vec.y); return *this;};
@@ -49,13 +106,29 @@ struct vec2 : public std::conditional<std::is_arithmetic<T>::value, IndependentV
     template <class Vector>
     vec2 operator-(const Vector& other) const {return vec2{this->x - other.x, this->y - other.y};};
     template<typename Number>
-    vec2 operator/(const Number divisor) const {return vec2(this->x / divisor, this->y / divisor);};
-    template<typename Number>
     vec2 operator*(const Number multiplier) const {return vec2(this->x * multiplier, this->y * multiplier);};
+    template<typename Number>
+    vec2 operator/(const Number divisor) const {return vec2(this->x / divisor, this->y / divisor);};
 
+    template <class Vector>
+    vec2 operator+=(const Vector& vec) {this->x += vec.x; this->y += vec.y; return *this;};
+    template <class Vector>
+    vec2 operator-=(const Vector& vec) {this->x -= vec.x; this->y -= vec.y; return *this;};
+    template<typename Number>
+    vec2 operator*=(const Number multiplier) {this->x *= multiplier; this->y *= multiplier; return *this;};
+    template<typename Number>
+    vec2 operator/=(const Number divisor) {this->x /= divisor; this->y /= divisor; return *this;};
+
+    template <class Vector>
+    bool operator==(const Vector& vector) const {return this->x == vector.x && this->y == vector.y;};
+    template <class Vector>
+    bool operator!=(const Vector& vector) const {return this->x != vector.x || this->y != vector.y;};
+#endif
+
+#ifndef WITHOUT_MEMBER_FUNCTIONS
 
     inline vFloat norm() const {
-        return sqrtf(this->x * this->x + this->y * this->y);
+        return vSqrt(this->x * this->x + this->y * this->y);
     }
 
     inline vec2 unitVector() const {
@@ -63,12 +136,12 @@ struct vec2 : public std::conditional<std::is_arithmetic<T>::value, IndependentV
     }
 
     inline vFloat rectangleArea() const {
-        return fabs(this->x * this->y);
+        return vAbs(this->x * this->y);
     }
 
     //Angle in radians
     inline vFloat angle() const {
-        return atan2f(this->y, this->x);
+        return vAtan2(this->y, this->x);
     }
 
     void scale(const vFloat factor) const {
@@ -78,15 +151,15 @@ struct vec2 : public std::conditional<std::is_arithmetic<T>::value, IndependentV
 
     void rotateRadians(const vFloat angle) {
         const auto oldX = this->x;
-        this->x = cosf(angle) * this->x - sinf(angle) * this->y;
-        this->y = sinf(angle) * oldX + cosf(angle) * this->y;
+        this->x = vCos(angle) * this->x - vSin(angle) * this->y;
+        this->y = vSin(angle) * oldX + vCos(angle) * this->y;
     }
 
     void multiplyComponents(const vec2& other) const {
         this->x *= other.x;
         this->y *= other.y;
     }
-
+#endif
 };
 using vec2f = vec2<vFloat>;
 using vec2i = vec2<int>;
@@ -97,84 +170,104 @@ std::ostream &operator<<(std::ostream &os, const vec2<T>& vector) {
     return os << "(" << std::to_string(vector.x) << ", " << std::to_string(vector.y) << ")";
 }
 
-namespace Vector{
+#ifndef WITHOUT_VECTOR_FUNCTIONS
+// Static Functions
+namespace vectoo {
 
-    template <class Vector>
-    static inline vFloat norm(const Vector& vector) {
-        return sqrtf(vector.x * vector.x + vector.y * vector.y);
+    // Convert any vector struct into any other
+    template<class VectorA, class VectorB>
+    static inline VectorA convert(const VectorB &vector) {
+        return {vector.x, vector.y};
     }
 
-    template <class Vector>
-    static inline Vector unitVector(const Vector& vector) {
+    // Norm of any vector struct that has an x and y component
+    template<class Vector>
+    static inline vFloat norm(const Vector &vector) {
+        return vSqrt(vector.x * vector.x + vector.y * vector.y);
+    }
+
+    template<class Vector>
+    static inline Vector unitVector(const Vector &vector) {
         return vector / norm(vector);
     }
 
-    template <class VectorA, class VectorB>
-    static inline vFloat dotProduct(const VectorA& vector1, const VectorB& vector2) {
+    template<class VectorA, class VectorB>
+    static inline VectorA add(const VectorA &vector1, const VectorB &vector2) {
+        return {vector1.x + vector2.x, vector1.y + vector2.y};
+    }
+
+    template<class VectorA, class VectorB>
+    static inline VectorA subtract(const VectorA &vector1, const VectorB &vector2) {
+        return {vector1.x - vector2.x, vector1.y - vector2.y};
+    }
+
+    template<class VectorA, class VectorB>
+    static inline vFloat dotProduct(const VectorA &vector1, const VectorB &vector2) {
         return vector1.x * vector2.x + vector1.y * vector2.y;
     }
 
     // Component-wise multiplication
-    template <class VectorA, class VectorB>
-    static inline VectorA multiplyComponents(const VectorA& vector1, const VectorB& vector2) {
+    template<class VectorA, class VectorB>
+    static inline VectorA multiplyComponents(const VectorA &vector1, const VectorB &vector2) {
         return {vector1.x * vector2.x, vector1.y * vector2.x};
     }
 
     // Area of the rectangle formed by the vector
-    template <class Vector>
-    static inline vFloat rectangleArea(const Vector& vector) {
-        return fabs(vector.x * vector.y);
+    template<class Vector>
+    static inline vFloat rectangleArea(const Vector &vector) {
+        return vAbs(vector.x * vector.y);
     }
 
-    template <class VectorA, class VectorB>
-    static inline vFloat rhombusArea(const VectorA& vector1, const VectorB& vector2) {
-        return fabs(vector1.x * vector2.y - vector1.y * vector2.x);
+    template<class VectorA, class VectorB>
+    static inline vFloat rhombusArea(const VectorA &vector1, const VectorB &vector2) {
+        return vAbs(vector1.x * vector2.y - vector1.y * vector2.x);
     }
 
     // Area of the triangle between the two vectors
-    template <class VectorA, class VectorB>
-    static inline vFloat triangleArea(const VectorA& vector1, const VectorB& vector2) {
+    template<class VectorA, class VectorB>
+    static inline vFloat triangleArea(const VectorA &vector1, const VectorB &vector2) {
         return rhombusArea(vector1, vector2) / 2;
     }
 
     // Signed area used in centroid calculations that use positive and negative areas
-    template <class VectorA, class VectorB>
-    static inline vFloat signedArea(const VectorA& vector1, const VectorB& vector2) {
+    template<class VectorA, class VectorB>
+    static inline vFloat signedArea(const VectorA &vector1, const VectorB &vector2) {
         return vector1.x * vector2.y - vector1.y * vector2.x;
     }
 
-    template <class Vector>
-    static inline vFloat vectorAngleRadians(const Vector& vector) {
-        return atan2f(vector.y, vector.x);
+    template<class Vector>
+    static inline vFloat vectorAngleRadians(const Vector &vector) {
+        return vAtan2(vector.y, vector.x);
     }
 
-    template <class Vector>
-    static inline Vector rotateVectorRadians(const Vector& vector, vFloat angle) {
-        return {cosf(angle) * vector.x - sinf(angle) * vector.y, sinf(angle) * vector.x + cosf(angle) * vector.y};
+    template<class Vector>
+    static inline Vector rotateVectorRadians(const Vector &vector, vFloat angle) {
+        return {vCos(angle) * vector.x - vSin(angle) * vector.y, vSin(angle) * vector.x + vCos(angle) * vector.y};
     }
 
     //Angle is unsigned in radians
-    template <class VectorA, class VectorB>
-    static inline vFloat angleBetweenVectors(const VectorA& vector1, const VectorB& vector2) {
-        return acosf(dotProduct(vector1, vector2) / (norm(vector1) * norm(vector2)));
+    template<class VectorA, class VectorB>
+    static inline vFloat angleBetweenVectors(const VectorA &vector1, const VectorB &vector2) {
+        return vAcos(dotProduct(vector1, vector2) / (norm(vector1) * norm(vector2)));
     }
 
     //linear interpolation
-    template <class VectorA, class VectorB>
-    static inline VectorA lerp(const VectorA& startPoint, const VectorB& endPoint, const vFloat alpha) {
+    template<class VectorA, class VectorB>
+    static inline VectorA lerp(const VectorA &startPoint, const VectorB &endPoint, const vFloat alpha) {
         return {startPoint.x * (1 - alpha) + endPoint.x * alpha, startPoint.y * (1 - alpha) + endPoint.y * alpha};
     }
 
     //faster than reflect() but it expects the normal to be a unit vector
-    template <class VectorA, class VectorB>
-    static inline VectorA reflectUnit(const VectorA& vector, const VectorB& normal) {
+    template<class VectorA, class VectorB>
+    static inline VectorA reflectUnit(const VectorA &vector, const VectorB &normal) {
         return vector - normal * 2 * dotProduct(vector, normal);
     }
 
     //reflects the vector off the normal vector
-    template <class VectorA, class VectorB>
-    static inline VectorA reflect(const VectorA& vector, const VectorB& normal) {
+    template<class VectorA, class VectorB>
+    static inline VectorA reflect(const VectorA &vector, const VectorB &normal) {
         const auto unitNormal = unitVector(normal);
         return reflectUnit(vector, unitNormal);
     }
 }
+#endif
